cbf0113cd5362146cb7ce07402b00105d80372db
heteddy <heteddy@126.com>
2020-12-31 17:18:28 +0800

    修改名字

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/yanyiwu/gojieba
+module github.com/heteddy/gojieba
 
 go 1.13
+
+require github.com/yanyiwu/gojieba v1.1.2 // indirect
--- init.go
+++ init.go
@@ -1,9 +1,9 @@
 package gojieba
 
 import (
-	"github.com/yanyiwu/gojieba/deps/cppjieba"
-	"github.com/yanyiwu/gojieba/deps/limonp"
-	"github.com/yanyiwu/gojieba/dict"
+	"github.com/heteddy/gojieba/deps/cppjieba"
+	"github.com/heteddy/gojieba/deps/limonp"
+	"github.com/heteddy/gojieba/dict"
 )
 
 func init() {
